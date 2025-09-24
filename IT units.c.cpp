64ed7807256c293c4a6dbc7c61ae3b6b2fc4d12c
tabbie <tabithamunuhe4@gmail.com>
2025-09-24 21:13:42 +0300

    Add files via upload

---
+++ IT units.c.cpp
@@ -0,0 +1,20 @@
+//simple c program 
+/*
+Name:Tabitha Muthoni
+Reg:CT100/G/26159/25
+Description:units covered in Bachelors of Science in Information Technology semester one
+*/
+
+#include<stdio.h>
+
+int main() {
+    printf("analogue electronics\n");
+	printf("introduction to programming and algorithms\n");
+	printf("computer systems and organisation\n");
+	printf("introduction to computer application\n");
+	printf("essentials of accounting\n");
+	printf("mathematics for science\n");
+	printf("fundamentals of ICT\n");
+	printf("communication skills\n");
+	return 0;
+}
---
+++ prompt profile.c.cpp
@@ -0,0 +1,26 @@
+// simple c program
+/*
+Name:Tabitha Muthoni
+Reg:CT100/G/26159/25
+Description:Profile
+*/
+
+#include<stdio.h>
+
+int main() {
+	int contact,age;
+	float height, weight;
+	
+	printf("enter your contact:\n");
+	scanf("%d",& contact);
+	
+	printf("enter your age:\n");
+	scanf("%d", & age);
+	
+	printf("enter your height:\n");
+	scanf("%f",& height);
+	
+	printf("enter your weight:\n");
+	scanf("%f",& weight);
+	return 0;
+}
