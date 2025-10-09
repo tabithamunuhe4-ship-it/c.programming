93a170dab68bc529f61f5669b72100346d062597
tabbie <tabithamunuhe4@gmail.com>
2025-10-09 10:38:33 +0300

    Add files via upload

---
+++ for_loop.c
@@ -0,0 +1,20 @@
+/*
+Name:Tabitha Muthoni
+Reg No:CT100/G/26159/25
+Description:printing program
+For loop in C
+program to print 100-50
+*/
+
+#include <stdio.h>
+
+int main() {
+	
+	//for(start; stop; step){}
+	int i;
+	for(i=100; i>=50; i--){
+		printf("%d \n", i);
+	}
+	
+	return 0;
+	}
