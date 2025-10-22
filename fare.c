01a5428112e4dd8c8b68f59d308e5be21f05bfa3
tabbie <tabithamunuhe4@gmail.com>
2025-10-22 23:19:15 +0300

    Refactor fare.c with consistent formatting

--- fare.c
+++ fare.c
@@ -4,27 +4,27 @@ Reg: CT100/G/26159/25
 Description: Fare Calculation
 */
 
+//Function to calculate fare
+
 #include <stdio.h>
 
-// Function prototype
+//function prototype
 float calculateFare(float distance);
 
-void main() {
+void main()
+{
     float result, distance;
-    
     printf("Enter distance traveled (in km): \t");
     scanf("%f", &distance);
     
-    // Function call
+    //function call
     result = calculateFare(distance);
     
     printf("The total fare is KSh. %.2f \n", result);
-    
-    return 0;
 }
 
-// Function declaration
-float calculateFare(float distance) {
+//Function declaration
+float calculateFare(float distance){
     float total_fare;
     total_fare = distance * 50;
     return total_fare;
